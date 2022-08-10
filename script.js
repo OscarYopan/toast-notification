331445fa96f40a7ec73b782d1ca5e2d3fca80256
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-08-10 15:17:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 const button = document.querySelector('.btn')
+const toasts = document.querySelector('#toasts')
 
+const messages = ['Call Somebody', 'Send E-mail', 'Visit Family', 'Refresh Buy List', 'Study', 'Dinners Time', 'Coffe Brake', 'Listen Music', 'Back Home']
+
+button.addEventListener('click', createNotification)
+
+function createNotification () {
+  const notif = document.createElement('div')
+  notif.classList.add('toast')
+  
+  notif.innerText = getRandomMessage()
+
+  toasts.appendChild(notif)
+
+  setTimeout(() => {
+    notif.remove()
+  }, 3000)
+}
+
+function getRandomMessage () {
+  return messages[Math.floor(Math.random() * messages.length)]
+}
